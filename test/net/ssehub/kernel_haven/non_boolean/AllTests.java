ddb3a9b09e8cb9257d94cab025a62391090fab35
Adam <adam@sse.uni-hildesheim.de>
2017-10-07 22:42:45 +0200

    Move NonBooleanPreparation into this separate plugin

---
+++ test/net/ssehub/kernel_haven/non_boolean/AllTests.java
@@ -0,0 +1,17 @@
+package net.ssehub.kernel_haven.non_boolean;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * All tests.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+    CPPUtilsTest.class,
+    NonBooleanPreperationTest.class,
+})
+public class AllTests {
+
+}
