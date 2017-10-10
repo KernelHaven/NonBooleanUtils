ffa436889b3155e9d920948f1ef5d7592b45f580
Adam <adam@sse.uni-hildesheim.de>
2017-10-10 14:03:40 +0200

    Expand NumberUtilsTest a tiny bit

--- test/net/ssehub/kernel_haven/non_boolean/NumberUtilsTest.java
+++ test/net/ssehub/kernel_haven/non_boolean/NumberUtilsTest.java
@@ -15,8 +15,8 @@ public class NumberUtilsTest {
      */
     @Test
     public void testParsingIntegers() {
-        String[] validIntegers = {"10", "100.0", "0x3e8", "0x44C"};
-        int[] expectedValues = {10, 100, 1000, 1100 };
+        String[] validIntegers = {"10", "100.0", "0x3e8", "0x44C", "0xFF"};
+        int[] expectedValues = {10, 100, 1000, 1100, 255 };
         
         for (int i = 0; i < validIntegers.length; i++) {
             Number parseResult = NumberUtils.convertToNumber(validIntegers[i]);
