cb17903554c0636e5597fb1cf58e1e0454adc618
Adam <adam@sse.uni-hildesheim.de>
2017-10-10 12:12:57 +0200

    Also check for expected values in NumberUtilsTest

--- test/net/ssehub/kernel_haven/non_boolean/NumberUtilsTest.java
+++ test/net/ssehub/kernel_haven/non_boolean/NumberUtilsTest.java
@@ -16,12 +16,14 @@ public class NumberUtilsTest {
     @Test
     public void testParsingIntegers() {
         String[] validIntegers = {"10", "100.0", "0x3e8", "0x44C"};
+        int[] expectedValues = {10, 100, 1000, 1100 };
         
         for (int i = 0; i < validIntegers.length; i++) {
             Number parseResult = NumberUtils.convertToNumber(validIntegers[i]);
             
             Assert.assertNotNull("Parsing of " + validIntegers[i] + " failed.", parseResult);
             Assert.assertTrue(parseResult instanceof Integer);
+            Assert.assertEquals(expectedValues[i], parseResult);
         }
     }
 
