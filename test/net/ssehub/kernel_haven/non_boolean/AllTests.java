556e14461c65ef9266f96d371f531d9b40ebbe01
Adam <adam@sse.uni-hildesheim.de>
2018-03-16 18:07:16 +0100

    Move CppParser to main infrastructure

--- test/net/ssehub/kernel_haven/non_boolean/AllTests.java
+++ test/net/ssehub/kernel_haven/non_boolean/AllTests.java
@@ -5,7 +5,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import net.ssehub.kernel_haven.non_boolean.heuristic.AllHeuristicTests;
-import net.ssehub.kernel_haven.non_boolean.parser.AllParserTests;
 import net.ssehub.kernel_haven.non_boolean.replacer.AllReplacerTests;
 
 /**
@@ -13,13 +12,11 @@ import net.ssehub.kernel_haven.non_boolean.replacer.AllReplacerTests;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-    AllParserTests.class,
     AllReplacerTests.class,
     AllHeuristicTests.class,
     
     CPPUtilsTest.class,
     NonBooleanPreperationScenarioTest.class,
-    NumberUtilsTest.class,
     CppBufferedWriterTest.class,
     NonBooleanPreparationTest.class,
     })
